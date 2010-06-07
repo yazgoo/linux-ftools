fc2617f2db67c48d6ecf769c92677629330aa678
Kevin Burton <burton@spinn3r.com>
2010-06-06 18:42:46 -0700

    test

--- fincore.c
+++ fincore.c
@@ -75,22 +75,14 @@ void fincore(char* path, int pages, int summarize, int only_cached, struct finco
 
     // TODO: make all these variables long and print them as ld
 
-    //ceil( 0.6 );
-    
-    //double required_pages = (double)file_stat.st_size / (double)page_size;
-    
-    double required_pages = ceil( 0.6 );
+    int total_pages = (int)ceil( (double)file_stat.st_size / (double)page_size );
 
-    int foobar = (int)ceil( required_pages );
+/*     if ( foobar != -100.0 ) { */
+/*         printf( "hello world\n" ); */
+/*     } */
 
-    printf( "FIXME1\n" );
-
-    if ( foobar != -100.0 ) {
-        printf( "hello world\n" );
-    }
-
-    int total_pages = file_stat.st_size / page_size;
-    //int total_pages = file_stat.st_blocks;
+/*     int total_pages = file_stat.st_size / page_size; */
+/*     //int total_pages = file_stat.st_blocks; */
 
     double cached_perc = 100 * (cached / (double)total_pages); 
 
