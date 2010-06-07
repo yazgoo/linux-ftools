1e7b18afd338285bdc2f5c6a46f51c1b2ebc2043
Kevin Burton <burton@spinn3r.com>
2010-06-06 18:34:13 -0700

    test

--- fincore.c
+++ fincore.c
@@ -77,7 +77,9 @@ void fincore(char* path, int pages, int summarize, int only_cached, struct finco
 
     ceil( 0.6 );
 
-    int total_pages = ceil( (double)file_stat.st_size / (double)page_size );
+    double required_pages = (double)file_stat.st_size / (double)page_size;
+
+    int total_pages = ceil( required_pages );
     //int total_pages = file_stat.st_blocks;
 
     double cached_perc = 100 * (cached / (double)total_pages); 
