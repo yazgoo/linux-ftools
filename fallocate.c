22579d94cb004c2911ae4988d7ab7a2b8308dde2
Kevin Burton <burton@spinn3r.com>
2010-06-06 17:51:02 -0700

    header file

--- fallocate.c
+++ fallocate.c
@@ -45,7 +45,8 @@ larger range than that which was specified.
 int main(int argc, char *argv[]) {
 
     if ( argc != 3 ) {
-        fprintf( stderr, "SYNTAX: fallocate file length" );
+        fprintf( stderr, sprintf( "%s version %s\n", argv[0], LINUX_FTOOLS_VERSION ) );
+        fprintf( stderr, "SYNTAX: fallocate file length\n" );
         return 1;
     }
 
--- linux-ftools.h
+++ linux-ftools.h
@@ -1,2 +1,2 @@
-
+// FIXME: READ the @VERSION from configure.ac 
 #define LINUX_FTOOLS_VERSION "1.0.0"
