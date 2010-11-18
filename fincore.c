f89c0efc2d73abce4c1a9bbddf3d4fb9399992b5
Kevin Burton <burton@spinn3r.com>
2010-11-18 12:29:42 -0800

    format

--- fincore.c
+++ fincore.c
@@ -114,7 +114,7 @@ void fincore(char* path,
         */
         //sprintf( "%s", file_stat.st_size );
 
-        sprintf( "%s", foobar( 100 ) );
+        foobar( 100 );
 
         printf( "%-120s \n",
                 path
