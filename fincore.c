1c30cc4b40674e532665067588e669539b1f8f79
Kevin Burton <burton@spinn3r.com>
2010-11-19 09:44:45 -0800

    format

--- fincore.c
+++ fincore.c
@@ -153,7 +153,7 @@ void fincore(char* path,
     int cached = 0;
     int printed = 0;
 
-    int nr_regions = 10;
+    int nr_regions = 80;
 
     long *regions = calloc( nr_regions , sizeof(regions) ) ;
 
