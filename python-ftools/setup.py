89aa859d21d61e548849c7bac85920ce412019eb
David Stainton <dstainton415@gmail.com>
2010-11-24 23:42:07 +0000

    python-ftools: bump version and fix name in setup.py

--- python-ftools/setup.py
+++ python-ftools/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
-setup(name='python-ftools',
-    version='0.1',
+setup(name='ftools',
+    version='0.2.0',
     author='David Stainton',
     author_email='dstainton415@gmail.com',
     ext_modules=[
