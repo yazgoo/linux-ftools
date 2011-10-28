5fd1116adad6fadfb4c37bf902b100d955bed948
Kevin Burton <burton@spinn3r.com>
2011-10-28 16:59:36 -0700

    ...

--- waste_memory.c
+++ waste_memory.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
     if ( argc == 2 ) 
         len = atol( argv[1] );
 
-    printf( "Allocating %l bytes ...", len );
+    printf( "Allocating %ll bytes ...", len );
 
     void *buff = malloc( len );
 
